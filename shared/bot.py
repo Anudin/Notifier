17f62eca9745c7e495b42af884107da7c6b4310d
Bastian Böhm <contact.bastianboehm@gmail.com>
2021-01-16 22:43:18 +0100

    Allow arbitrary named arguments for (bot) send_message

--- shared/bot.py
+++ shared/bot.py
@@ -17,10 +17,10 @@ def read_config(path) -> BotConfig:
         return BotConfig(config["token"], config.get("chatId"))
 
 
-def send_message(bot, chat_id, message):
+def send_message(bot, chat_id, message, **kwargs):
     from telegram.error import TelegramError
 
     try:
-        bot.sendMessage(chat_id=chat_id, text=message)
+        bot.sendMessage(chat_id=chat_id, text=message, **kwargs)
     except TelegramError as e:
         print(f"Failed to send Telegram message { e }")
