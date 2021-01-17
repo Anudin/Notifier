f7f0d3c9afa10c69ef5d7c892c8237d4284daaee
Bastian Böhm <contact.bastianboehm@gmail.com>
2021-01-17 12:07:06 +0100

    util: optionally restrict messages to a list of chats

--- shared/bot.py
+++ shared/bot.py
@@ -1,10 +1,13 @@
+from telegram import Bot
 from dataclasses import dataclass
 from typing import Optional
 
 
-# TODO chat_id should be Optional[list[str]]
+# TODO chat_id should be Optional[list[str]], adjust depending code
 @dataclass
 class BotConfig:
+    """Static configuration data for a Telegram bot, optionally specifying a fixed list of chats to work with."""
+
     token: str
     chat_id: Optional[str]
 
@@ -17,10 +20,15 @@ def read_config(path) -> BotConfig:
         return BotConfig(config["token"], config.get("chatId"))
 
 
-def send_message(bot, chat_id, message, **kwargs):
+# TODO Check out functools.wraps
+def send_message(bot: Bot, chat_id: str, message: str, config: BotConfig = None, **kwargs):
+    """Fire and forget Telegram message sending. Swallows errors, optionally restricts allowed chats."""
     from telegram.error import TelegramError
+
+    if config and chat_id != config.chat_id:
+        return print(f"Tried sending a message to {chat_id}, not allowed")
 
     try:
         return bot.sendMessage(chat_id=chat_id, text=message, **kwargs)
     except TelegramError as e:
-        print(f"Failed to send Telegram message { e }")
+        return print(f"Failed to send Telegram message { e }")
